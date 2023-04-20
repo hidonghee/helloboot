f8b1af16afa0d6c38a91aeeafba300fea08e7a85
hidonghee <tlsehdgml159@gmail.com>
2023-04-20 17:40:12 +0900

    springboot 초기상태를 직접 hands-on해서 구현

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,7 +1,15 @@
 package tobyspring.helloboot;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.Objects;
 
+@RestController
 public class HelloController {
     private final HelloService helloService;
 
@@ -9,6 +17,7 @@ public class HelloController {
         this.helloService = helloService;
     }
 
+    @GetMapping("/hello")
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
---
+++ src/main/java/tobyspring/helloboot/mySpringApplication.java
@@ -0,0 +1,31 @@
+package tobyspring.helloboot;
+
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class mySpringApplication {
+   public static void run(Class<?> applicationClass, String ...args){
+        //스프링 컨테이너 만들기
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext(){
+            @Override
+            protected void onRefresh(){
+                super.onRefresh();
+
+                ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
+                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
+                dispatcherServlet.setApplicationContext(this);
+
+                WebServer webServer = serverFactory.getWebServer(servletContext -> {
+                    servletContext.addServlet("dispatcherServlet",
+                            new DispatcherServlet(this) //자기 자신을 참조 this
+                    ).addMapping("/*");
+                });
+                webServer.start();
+            }
+        };
+        applicationContext.register(applicationClass);
+        applicationContext.refresh();  //빈이 생성되기 전에 컨테이너를 초기화하는 작업
+    }
+}
